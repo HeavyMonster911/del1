15f434d668ebebb177497d2bc0882eb63bd23f94
HeavyMonster911 <72610595+HeavyMonster911@users.noreply.github.com>
2021-03-22 13:17:25 +0200

    Create animeJS.js

---
+++ animeJS.js
@@ -0,0 +1,27 @@
+// const container = document.querySelector(".backgroundContainer")
+// for(var i = 0; i <= 100; i++) {
+//     const blocks = document.createElement('div');
+//     blocks.classList.add('block');
+//     container.appendChild(blocks)
+// }
+
+// function animateBlocks() {
+//     anime({
+//         targets: '.block',
+//         translateX: function() {
+//             return anime.random(-500,500)
+//         },
+//         translateY: function() {
+//             return anime.random(-100, 2100)
+//         },
+//         scale: function() {
+//             return anime.random(1,5)
+//         },
+
+//         easing: 'linear',
+//         duration: 3000,
+//         delay: anime.stagger(10),
+//     })
+// }
+
+// animateBlocks()
